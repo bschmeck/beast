d138c9e660a0dba1756f4db242af4b3ab35f6fe4
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2011-10-06 09:05:42 -0400

    First iteration on models.

--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from datetime import date, time
+
+class Workout(models.Model):
+    start = models.DateTimeField()
+    warmup = models.TimeField()
+    location = models.CharField(max_length=128)
+    description = models.TextField()
+    organizer = models.ForeignKey(User, related_name='organzied_workouts')
+    confirmed = models.ManyToManyField(User, related_name='confirmed_workouts')
+    interested = models.ManyToManyField(User, related_name='possible_workouts')
+    tags = models.ManyToManyField('Tag')
+    title = models.CharField(max_length=50)
+
+class Tag(models.Model):
+    text = models.CharField(max_length=50)
+
+
+
+    
