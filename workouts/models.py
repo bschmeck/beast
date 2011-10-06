f33e2741fa661e30056c834a6e093b5f3673383c
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2011-10-06 09:59:01 -0400

    Some fields in Workout should be optional.

--- workouts/models.py
+++ workouts/models.py
@@ -6,13 +6,13 @@ from datetime import date, time
 class Workout(models.Model):
     startDate = models.DateField()
     startTime = models.TimeField()
-    warmupTime = models.TimeField()
+    warmupTime = models.TimeField(blank=True)
     location = models.CharField(max_length=128)
     description = models.TextField()
     organizer = models.ForeignKey(User, related_name='organzied_workouts')
-    confirmed = models.ManyToManyField(User, related_name='confirmed_workouts')
-    interested = models.ManyToManyField(User, related_name='possible_workouts')
-    tags = models.ManyToManyField('Tag')
+    confirmed = models.ManyToManyField(User, related_name='confirmed_workouts', blank=True)
+    interested = models.ManyToManyField(User, related_name='possible_workouts', blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
     title = models.CharField(max_length=50)
 
 class Tag(models.Model):
