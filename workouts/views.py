0b72d5e370e399935c29ef2a5a355af711fb6976
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2011-10-11 23:03:18 -0400

    Need to import django's mail.

--- workouts/views.py
+++ workouts/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core import mail
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import Context, RequestContext
