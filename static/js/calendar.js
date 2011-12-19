b494c4329ca11838968dcfed94f69b3f528f5b94
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2011-12-19 16:49:35 -0500

    Use 'twipsies' instead of alerts for known workout locations

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -69,6 +69,9 @@ function buildCalendar() {
 	                    $(this).toggleClass("highlight default");
 		            });
 	            });
+                $("a[rel=twipsy]").twipsy({
+                    live: true
+                });
             $(".workout_action").click(function(event) {
                 event.preventDefault();
                 var action = event.target.id;
