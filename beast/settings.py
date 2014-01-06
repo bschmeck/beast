8437d275e98375e21e066fd4744389ca213de14a
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2014-01-05 22:17:10 -0600

    Configure the ALLOWED_HOST setting

--- beast/settings.py
+++ beast/settings.py
@@ -25,6 +25,10 @@ DATABASES = {
     }
 }
 
+ALLOWED_HOSTS = [
+    'beast.shmk.org',
+    ]
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
