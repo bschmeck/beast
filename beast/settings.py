617fb2363f0d1f668d4e11b80e1bfa4a1722bd42
Ben Schmeckpeper <ben@aisle50.com>
2014-01-07 09:07:04 -0600

    Add south to the project

--- beast/settings.py
+++ beast/settings.py
@@ -125,7 +125,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'workouts'
+    'workouts',
+    'south'
 )
 
 # A sample logging configuration. The only tangible logging
