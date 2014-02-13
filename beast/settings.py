10b6becc1658e19e9fb9bfacb40ea99137b13562
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2014-02-12 23:57:53 -0600

    MEDIA_ROOT and STATIC_ROOT need to be different

--- beast/settings.py
+++ beast/settings.py
@@ -54,7 +54,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/bschmeck/webapps/beast_static'
+MEDIA_ROOT = '/home/bschmeck/webapps/beast_static/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
