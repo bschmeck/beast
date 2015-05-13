7d2b0f8b33cb3c91fcc71f767b7b663ed5356e6f
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-05-12 22:17:59 -0500

    Revert "first() is not a thing you can do in Django"
    This reverts commit 32aad4129986e2bc1edf2e374a1272994f0fd41e.

--- beast/workouts/views.py
+++ beast/workouts/views.py
@@ -280,7 +280,7 @@ def calendar(request, slug=None):
     else:
         weekStart = 6
         if not city:
-            city = City.objects.all()[0]
+            city = City.objects.first()
     alt_cities = City.objects.exclude(id=city.id)
     alt_cities = [alt_cities[i:i+2] for i in range(0, len(alt_cities), 2)]
     
